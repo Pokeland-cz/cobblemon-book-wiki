9235f632544874c1bc99b329e89c3dc5bfd2860a
vaniron <hrdlicka.luk@gmail.com>
2025-07-10 22:19:47 +0200

    removed unneeded dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,6 @@ dependencies {
     modImplementation("com.gitlab.aglarcz.cobblemon:fabric:c1ce5491")
 
     modImplementation("eu.pb4:sgui:1.6.1+1.21.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 }
 
 
